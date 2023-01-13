5ea248d2a21f90112c42d6f18c02a3bcab24bb13
rsippl <260389+rsippl@users.noreply.github.com>
2023-01-13 18:21:29 +0100

    Inject logger and dateFormatter fields into fragments (with transitive dependencies)

--- app/src/main/java/com/example/android/hilt/ServiceLocator.kt
+++ app/src/main/java/com/example/android/hilt/ServiceLocator.kt
@@ -16,28 +16,13 @@
 
 package com.example.android.hilt
 
-import android.content.Context
 import androidx.fragment.app.FragmentActivity
-import androidx.room.Room
-import com.example.android.hilt.data.AppDatabase
-import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.AppNavigatorImpl
-import com.example.android.hilt.util.DateFormatter
 
-class ServiceLocator(applicationContext: Context) {
+class ServiceLocator {
 
-    private val logsDatabase = Room.databaseBuilder(
-        applicationContext,
-        AppDatabase::class.java,
-        "logging.db"
-    ).build()
-
-    val loggerLocalDataSource = LoggerLocalDataSource(logsDatabase.logDao())
-
-    fun provideDateFormatter() = DateFormatter()
-
-    fun provideNavigator(activity: FragmentActivity): AppNavigator {
-        return AppNavigatorImpl(activity)
-    }
+  fun provideNavigator(activity: FragmentActivity): AppNavigator {
+    return AppNavigatorImpl(activity)
+  }
 }
--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -28,13 +28,16 @@ import com.example.android.hilt.R
 import com.example.android.hilt.data.LoggerLocalDataSource
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 /**
  * Fragment that displays buttons whose interactions are recorded.
  */
+@AndroidEntryPoint
 class ButtonsFragment : Fragment() {
 
-    private lateinit var logger: LoggerLocalDataSource
+    @Inject lateinit var logger: LoggerLocalDataSource
     private lateinit var navigator: AppNavigator
 
     override fun onCreateView(
@@ -52,9 +55,6 @@ class ButtonsFragment : Fragment() {
     }
 
     private fun populateFields(context: Context) {
-        logger = (context.applicationContext as LogApplication).
-            serviceLocator.loggerLocalDataSource
-
         navigator = (context.applicationContext as LogApplication).
             serviceLocator.provideNavigator(requireActivity())
     }
