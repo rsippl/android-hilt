4ba69d6e2002bebed5bb62297fc44ab44551a7d3
rsippl <260389+rsippl@users.noreply.github.com>
2023-01-14 00:33:33 +0100

    Use qualifiers to inject two classes implementing the same interface

--- app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/example/android/hilt/ui/ButtonsFragment.kt
@@ -16,16 +16,15 @@
 
 package com.example.android.hilt.ui
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.android.hilt.LogApplication
 import com.example.android.hilt.R
-import com.example.android.hilt.data.LoggerLocalDataSource
+import com.example.android.hilt.data.LoggerDataSource
+import com.example.android.hilt.di.InMemoryLogger
 import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.Screens
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,7 +36,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class ButtonsFragment : Fragment() {
 
-    @Inject lateinit var logger: LoggerLocalDataSource
+    @InMemoryLogger
+    @Inject lateinit var logger: LoggerDataSource
     @Inject lateinit var navigator: AppNavigator
 
     override fun onCreateView(
